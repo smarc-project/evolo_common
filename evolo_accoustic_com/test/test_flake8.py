f26a4f4f2cf5bee8d5c8be6ebac4361df1dc1a83
Kaveh <najafian@kth.se>
2026-06-06 15:53:16 +0200

    Add Evolo messaging to and from the Succorfish acoustic modem over serial_parser. Scheduling included

---
+++ evolo_accoustic_com/test/test_flake8.py
@@ -0,0 +1,10 @@
+from ament_flake8.main import main_with_errors
+import pytest
+
+
+@pytest.mark.flake8
+@pytest.mark.linter
+def test_flake8():
+    rc, errors = main_with_errors(argv=[])
+    assert rc == 0, \
+        'Found code style errors / warnings:\n' + '\n'.join(errors)
